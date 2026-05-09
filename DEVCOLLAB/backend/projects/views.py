9d498a7654991b7e55711f49044f59037cac58fe
Hema M <hemam6771@gmail.com>
2026-05-09 12:37:36 +0530

    Completed JWT auth and project API

---
+++ DEVCOLLAB/backend/projects/views.py
@@ -0,0 +1,13 @@
+
+from rest_framework import generics, permissions
+from .models import Project
+from .serializers import ProjectSerializer
+
+class ProjectListCreateView(generics.ListCreateAPIView):
+
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
